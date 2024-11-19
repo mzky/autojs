31c2d529fdc366cd3a307912387d2afba240c09f
池边树下 <mzky@163.com>
2024-11-19 13:40:20 +0800

    Create auto.js

---
+++ auto.js
@@ -0,0 +1,3 @@
+setInterval(() => { document.querySelector('.layui-layer-btn .layui-layer-btn0').click(); }, 10000);
+setInterval(() => { document.querySelector('#message_close').click(); }, 10000);
+setInterval(() => { player.play(); }, 10000);
